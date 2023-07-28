da9dc64238cd1f8d9b719904ec83ec3f11897404
Földvári Tamás <foldvaritamasf@gmail.com>
2023-07-28 21:31:15 +0200

    implement initial landing page with navbar

---
+++ visa-webapp/src/Components/LandingPage.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import BasicExample from './Navbar';
+
+export default function WelcomeMessage(){
+    //const [greeting, setGreeting] = useState('');
+
+
+    /*useEffect(() => {
+        fetch("https://localhost:7091/LandingPage")
+        .then(response => response.text())
+        .then(data => {
+            setGreeting(data)
+          })
+    }, []);*/
+
+
+    return (
+      <div>
+        <div><BasicExample/></div>        
+        <h1>Greeting{/* {greeting} */}</h1>
+      </div>
+    );  
+}
